bd78df53e5df60cfa9537e757afe9351352302d4
D <izzydman03@icloud.com>
2021-01-15 16:39:58 -0800

    bloop

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.86.112.93',
+      host: '3.88.229.178',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
